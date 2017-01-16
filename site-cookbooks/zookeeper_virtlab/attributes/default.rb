727c3033de65e6699e1d4f8bb8f16a41cb556e33
Gaston Gonzalez <gg@headwire.com>
2017-01-16 08:30:13 -0700

    Switched to Oracle JDK 8

--- site-cookbooks/zookeeper_virtlab/attributes/default.rb
+++ site-cookbooks/zookeeper_virtlab/attributes/default.rb
@@ -1,4 +1,4 @@
 # Use Oracle JDK 8
-#default['java']['install_flavor'] = 'oracle'
-#default['java']['jdk_version'] = '8'
-#default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = '8'
+default['java']['oracle']['accept_oracle_download_terms'] = true
