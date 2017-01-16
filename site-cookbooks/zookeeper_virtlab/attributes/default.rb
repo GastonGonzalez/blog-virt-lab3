4c2f7cd18802dda5ab02c6d70c364f01cdd5ae3d
Gaston Gonzalez <gg@headwire.com>
2017-01-16 10:21:22 -0700

    Override default log location

--- site-cookbooks/zookeeper_virtlab/attributes/default.rb
+++ site-cookbooks/zookeeper_virtlab/attributes/default.rb
@@ -8,3 +8,9 @@ default['zookeeper-cluster']['service']['version'] = '3.4.9'
 # Note: https://supermarket.chef.io/cookbooks/libartifact requires a SHA-256 hash.
 # Run shasum -a 256 zookeeper-3.4.9.tar.gz to determin the correct checksum.
 default['zookeeper-cluster']['service']['binary_checksum'] = 'e7f340412a61c7934b5143faef8d13529b29242ebfba2eba48169f4a8392f535'
+
+
+# Override default log configuration
+default['zookeeper-cluster']['log4j']['properties']['zookeeper.root.logger'] = 'INFO, ROLLINGFILE'
+default['zookeeper-cluster']['log4j']['properties']['zookeeper.log.dir'] = '/var/log/zookeeper'
+default['zookeeper-cluster']['log4j']['properties']['zookeeper.tracelog.dir'] = '/var/log/zookeeper'
